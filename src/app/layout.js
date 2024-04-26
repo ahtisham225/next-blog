3895531c562d69ba4aca0e04c790f7a2b9c4a5b8
ahtisham225 <ahtisham225ali@gmail.com>
2024-04-27 00:42:31 +0500

    Styles + Components Added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import './globals.css'
+import Navbar from '@/components/navbar/Navbar'
+import Footer from '@/components/footer/Footer'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='container'>  
+          <div className='wrapper'>
+            <Navbar/>
+              {children}
+            <Footer/>
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
